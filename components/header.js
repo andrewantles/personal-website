7ab99a8ff680dbcfda19d8ab1df5720a42f9b1f2
Andrew Antles <andrew.antles@outlook.com>
2023-10-05 11:22:05 -0700

    Add profile pic, favicon, visual formatting

--- components/header.js
+++ components/header.js
@@ -2,8 +2,17 @@
 
 function buildHeader () {
   header = document.getElementById("header");
+
+  let headerPic = document.createElement("img")
+  headerPic.setAttribute("id", "header-pic")
+  headerPic.setAttribute("src", "../public/profile.jpg")
+  headerPic.setAttribute("alt", "Profile picture headshot of Andrew Antles")
+
   let headerText = document.createElement("h1");
+  headerText.setAttribute("id", "header-text");
   headerText.innerHTML = "AndrewAntles.Net";
+  
+  header.appendChild(headerPic);
   header.appendChild(headerText);
 }
 
